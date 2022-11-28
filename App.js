86069a1d1b2477750400a2c43ca249cd4255ab52
Giovanni Lituma <giovanninet@icloud.com>
2022-11-28 16:38:08 -0500

    Add ScrollView

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './components/WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       }}>
       <LittleLemonHeader />
       <WelcomeScreen />
+      {/* <MenuItems /> */}
       <LittleLemonFooter />
     </View>
   );
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,24 +1,33 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
     <View style={{ flex: 0.8 }}>
-      <Text style={{
-        padding: 48,
-        fontSize: 32,
-        color: 'white',
-        textAlign: 'center',
-      }}>
-        Welcome to Little Lemon
-      </Text>
-      <Text style={{
-        padding: 40,
-        fontSize: 24,
-        color: 'white',
-        textAlign: 'center',
-      }}>
-        Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
-      </Text>
+      <ScrollView
+        indicatorStyle={"white"}
+        style={{
+          paddingHorizontal: 40,
+          paddingVertical: 40,
+          backgroundColor: "black",
+        }}>
+        <Text style={{
+          padding: 48,
+          fontSize: 48,
+          color: 'white',
+          textAlign: 'center',
+        }}>
+          Welcome to Little Lemon
+        </Text>
+        <Text style={{
+          marginVertical: 8,
+          padding: 40,
+          fontSize: 40,
+          color: 'white',
+          textAlign: 'center',
+        }}>
+          Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
+        </Text>
+      </ScrollView>
     </View>
   );
 }
